5f81dbe11e32e9f1621016a66089f9b5d63ae576
Anna Valeria Makaria <94723781+OnnaMcadva@users.noreply.github.com>
2023-11-20 14:09:37 +0100

    Update ft_memcpy.c

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: anmakaro <anmakaro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 12:07:32 by anmakaro          #+#    #+#             */
-/*   Updated: 2023/10/21 20:48:47 by anmakaro         ###   ########.fr       */
+/*   Updated: 2023/11/19 16:40:02 by anmakaro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,23 @@
 
 void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
-	char		*d;
-	const char	*s;
+	unsigned char		*d;
+	const unsigned char	*s;
 
+	if (n == 0)
+		return (dest);
 	d = dest;
 	s = src;
-	while (n-- > 0)
+	if (dest < src && src < dest + n)
 	{
-		*d++ = *s++;
+		ft_memmove(dest, src, n);
+	}
+	else
+	{
+		while (n-- > 0)
+		{
+			*d++ = *s++;
+		}
 	}
 	return (dest);
 }
