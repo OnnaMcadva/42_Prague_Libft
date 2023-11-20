e515b155aa7d513be197b43b53aaa9a73e965947
Anna Valeria Makaria <94723781+OnnaMcadva@users.noreply.github.com>
2023-11-20 14:11:49 +0100

    Update ft_strlcpy.c

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: anmakaro <anmakaro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 12:08:03 by anmakaro          #+#    #+#             */
-/*   Updated: 2023/10/21 20:51:18 by anmakaro         ###   ########.fr       */
+/*   Updated: 2023/11/19 16:41:00 by anmakaro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ size_t	ft_strlcpy(char *dest, const char *src, size_t size)
 	size_t	i;
 	size_t	src_len;
 
+	if (!dest || !src)
+		return (0);
 	i = 0;
 	src_len = 0;
 	while (src[src_len] != 0)
