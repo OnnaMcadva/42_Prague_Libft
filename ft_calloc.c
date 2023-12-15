178c6c56e8aa2096107342bf3b70796acd533858
Anna Valeria Makaria <94723781+OnnaMcadva@users.noreply.github.com>
2023-12-16 00:13:15 +0100

    Update ft_calloc.c

--- ft_calloc.c
+++ ft_calloc.c
@@ -21,8 +21,9 @@ void	*ft_calloc(size_t count, size_t size)
 	total_size = count * size;
 	if (count && total_size / count != size)
 		return (NULL);
-	//	if (nitems && (size_t)(-1) / nitems < size)
-	//	return (NULL);
+	//	if (count && (size_t)(-1) / count < size)
+	//		return (NULL);
+	// интересный вариант проверки на оверфлоу - списано у чувака из Школы)
 	ptr = malloc(total_size);
 	i = 0;
 	if (ptr == NULL)
